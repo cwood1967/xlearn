aaf62ef2689f9fb77c606888747eddbb86e1770a
Chris Wood <cjw@stowers.org>
2020-10-02 16:23:48 -0500

    moved length stuff out

--- cellfinder/test/test_infer.py
+++ cellfinder/test/test_infer.py
@@ -1,40 +1,42 @@
-# %%
-from math import inf
-import autoreload
-%load_ext autoreload
-%autoreload 2
-# %%
 
 import glob
+import numpy as np
 import tifffile
 import torch
+from matplotlib import pyplot as plt
 
-import infer
-import train
+from cellfinder import infer, train
 
-# %%
-tm = train.get_model(2)
-tm.load_state_dict(torch.load("Data/trained_model_20200930.pt"))
-# %%
+def run_test(model_file, rundir=False):
+    tm = train.get_model(2)
+    tm.load_state_dict(torch.load(model_file))
+    # %%
 
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
     
-files = sorted(glob.glob('Data/ND2Images/*.tif'))
-len(files)
-                
-# %%
-import time
-cnn = infer.predict(tm, size=(400, 400), probability=.95)
-for test in files:
-    x = tifffile.imread(test)
-    t1 = time.time()
+    gp = '/home/cjw/Code/xlearn/cellfinder/Data/ND2Images/*.tif'
+    files = sorted(glob.glob(gp))
+                    
+    print(len(files))
+    import time
+    cnn = infer.predict(tm, size=(400, 400), probability=.95)
+    rf = np.random.randint(0, len(files))
+    x = tifffile.imread(files[rf])
     r, b = cnn(x)
-    print(r.mean(), time.time() - t1)
-# %%
-infer.inferdir('Data/ND2Images/', 'Data/trained_model.pt', 'Data/SavedRes', '*.tif')
-# %%
-r.shape
-# %%
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(x) 
+    plt.subplot(1,2,2)
+    plt.imshow(r)    
+    # for test in files:
+    #     x = tifffile.imread(test)
+    #     t1 = time.time()
+    #     r, b = cnn(x)
+    #     print(r.mean(), time.time() - t1)
+    
+    if rundir:
+        infer.inferdir('Data/ND2Images/', 'Data/trained_model.pt', 'Data/SavedRes', '*.tif')
+
--- itest.py
+++ itest.py
@@ -1,3 +1,10 @@
+# %%
 import autoreload
 %load_ext autoreload
 %autoreload 2
+
+# %%
+from cellfinder.test import test_infer
+# %%
+test_infer.run_test("/home/cjw/Code/xlearn/cellfinder/Data/trained_model_20200930.pt")
+# %%
